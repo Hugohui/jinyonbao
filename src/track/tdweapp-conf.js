93fe1aa1f35c0bcacba20b40604052db0b573efd
huiwenhua <huiwenhua@liquidnetwork.com>
2020-03-01 10:55:49 +0800

    TD

---
+++ src/track/tdweapp-conf.js
@@ -0,0 +1,11 @@
+import config from '@/config/config'
+exports.config = {
+  appkey: 'CA09F709173E4327BFFF4501AC99989E',
+  appName: '进佣宝',
+  versionName: '1.1.4',
+  versionCode: '114',
+  wxAppid: 'wx5e2758ac42a14693',
+  getLocation: false, // 默认不获取用户位置
+  autoOnPullDownRefresh: false, // 默认不统计下拉刷新数据
+  autoOnReachBottom: false // 默认不统计页面触底数据
+};
